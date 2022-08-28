a0d8a41682da5a4bce7bfcffeb494b4563f5d4dc
OeKintaro <mystofrito@gmail.com>
2022-08-28 17:32:46 -0300

    Rotas e parâmetros

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app
+    .get('/users', (req, res) => {
+        const params = req.query;
+        res.json(params);
+        
+    });
 
 app
-    .get('/', (req, res) => {
-        res.end('<h1>Hello World!</h1>');
+    .post('/users/:id', (req, res) => {
+        const params = req.params;
+        res.json(params);
+        
+    });
+
+app
+    .post('/users/', (req, res) => {
+        const params = req.body;
+        res.json(params);
         
     });
 
